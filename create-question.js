23596201b1ce58ff15c72313102a4a286e540c6f
NguyenAnphong <43713980+NguyenAnphong@users.noreply.github.com>
2019-05-28 19:13:55 -0700

    Add files via upload

---
+++ create-question.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+
+console.log("js loaded");
+
+
+
+var acceptButton = document.getElementById("postQuestionButton");
+acceptButton.addEventListener('click', function () {
+    console.log('click');
+
+
+
+});
