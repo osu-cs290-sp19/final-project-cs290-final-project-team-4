b7f10f763ac70198cec2cd5803c5d126d1829c2b
Brian Cebra <cebrab@oregonstate.edu>
2019-06-04 11:50:39 -0700

    Made Links for each categories page

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 var path = require('path');
 var express = require('express');
+var exphbs = require('express-handlebars');
 //var exphbs = require('express-handlebars');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var questionData = require('./questionData');
+
+app.engine('handlebars', exphbs({ defaultLayout: false }));
+app.set('view engine', 'handlebars');
+
 app.use(express.static('public'));
+
+app.get('/categories/:cat_name', function (req, res, next) {
+  var category_name = req.params.cat_name.toLowerCase();
+  if (questionData[category_name]){
+     res.status(200).render('categories-list', questionData[category_name]);
+  } else {
+    next();
+  }
+});
 
 app.get('/',function(req, res, next) {
   res.status(200).sendFile(__dirname + '/public/home.html');
