038ea17912601d66183f640a15b296896bb3a8a0
NguyenAnphong <43713980+NguyenAnphong@users.noreply.github.com>
2019-06-04 18:31:05 -0700

    Add files via upload
    Added some handlebars responses

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var path = require('path');
 var express = require('express');
-//var exphbs = require('express-handlebars');
+var exphbs = require('express-handlebars');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+var questions = require('questionData');
+
 
 app.use(express.static('public'));
 
@@ -11,16 +17,22 @@ app.get('/',function(req, res, next) {
   res.status(200).sendFile(__dirname + '/public/home.html');
 });
 
-app.get('/', function(req, res, next) {
-  res.status(200).sendFile(__dirname + '/public/create-question.html');
+app.get('/create_question', function(req, res, next) {
+    res.status(200).render('createQuestion');
 });
 
-app.get('/', function(req, res, next) {
-  res.status(200).sendFile(__dirname + '/public/answer_question.html');
+app.get('/answer_question/:category/:number', function(req, res, next) {
+    var question = questions[req.params.category][req.params.number];
+    res.status(200).render('answerQuestion', question);
 });
 
+app.get('/answer_question/:category', function (req, res, next) {
+    var questions = questions[req.params.category];
+    res.status(200).render('listOfQuestions', questions);
+})
+
 app.get('/', function(req, res, next) {
-  res.status(200).sendFile(__dirname + 'public/categories.html');
+    res.status(200).sendFile(__dirname + 'public/categories.html');
 });
 
 app.listen(PORT, function(err) {
