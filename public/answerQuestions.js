c5dec97799e5831d8bc63605fbd15415422f79d6
Anphong Nguyen <nguyeanp@flip1.engr.oregonstate.edu>
2019-06-10 18:35:07 -0700

    Added answer question functionality to homepage

--- public/answerQuestions.js
+++ public/answerQuestions.js
@@ -3,10 +3,25 @@ console.log("answerQuestions.js loaded");
 var answerChoices = document.getElementsByClassName('answer-choice-event-button');
 var answerStatsBoxes = document.getElementsByClassName('answer-stats-box');
 var dontAnswer = document.querySelector('.dont-answer-button');
+var homePageQIndex = document.getElementsByClassName('HPQuestionIndex');    /*  array of question indices*/
+var homePageQLength = document.getElementsByClassName('HPQuestionLength');  /*  array of # choices per question*/
+var homePageQuestions = document.getElementsByClassName('homepageQuestion');        /*  array of qeustions*/
+var homePageAnswers = document.getElementsByClassName('homepageAnswer');     /*  array of all answers (not segmented by questions*/
+var homeCovers = document.getElementsByClassName('homeQuestionFiller');
+
+for (var z = 0; z < homePageQIndex.length; z++) {
+    homePageQIndex[z].classList.add('hidden');
+}
+
+for (var x = 0; x < homePageQLength.length; x++) {
+    homePageQLength[x].classList.add('hidden');
+}
 
 for (var y = 0; y < answerStatsBoxes.length; y++){
   answerStatsBoxes[y].classList.add('hidden');
 }
+
+var catList = ["sports", "politics", "food", "media", "wyr", "lifestyle", "misc"];
 
 function changeDBStats(answerNumber){
   var newAnswerCategory = null, questionNumber = null;
@@ -23,11 +38,25 @@ function changeDBStats(answerNumber){
   request.send();
 }
 
+function changeDBStatsHome(catNumber, choiceNumber) {
+    var questionCategory = catList[catNumber];
+    var questionNumber = homePageQIndex[catNumber].innerHTML;
+    var request = new XMLHttpRequest();
+    var url = '/answer_question/' + questionCategory + '/' + questionNumber + '/' + choiceNumber;
+    console.log("url = ", url);
+    request.open('POST', url);
+    request.send();
+}
+
 function choicesToStats(){
-  for (var x = 0; x < answerStatsBoxes.length; x++){
-    answerChoices[x].classList.toggle('hidden');
-    answerStatsBoxes[x].classList.toggle('hidden');
-    dontAnswer.textContent = "Next Question";
+    for (var x = 0; x < answerStatsBoxes.length; x++) {
+        if (answerChoices) {
+            answerChoices[x].classList.toggle('hidden');
+        }
+        answerStatsBoxes[x].classList.toggle('hidden');
+        if (dontAnswer) {
+            dontAnswer.textContent = "Next Question";
+        }
   }
 }
 
@@ -38,15 +67,28 @@ function updateStatDisplay(questionNumber){
   answerStatsBoxes[questionNumber].querySelector('.percentage').textContent = answerPercentage;
 }
 
+function answerSelectedHP(event) {
+    var k = 0;
+    for (var i = 0; i < homePageQuestions.length; i++) {
+        for (var j = 0; j < Number(homePageQLength[i].innerHTML); j++) {
+            if (event.target == answerChoices[k]) {
+                console.log("Answer ", j, " of category", i, "selected");
+                changeDBStatsHome(i, j);
+                homePageQuestions[i].classList.add('hidden');
+                homeCovers[i].classList.remove('hidden');
+            }
+            k++;
+        }
+    }
+}
+
 function answerSelected (event){
   for (var i = 0; i < answerChoices.length; i++){
     if (event.target == answerChoices[i]){
       console.log("You have selected answer choice ", i);
-      if (answerStatsBoxes){
         updateStatDisplay(i);
         choicesToStats();
         changeDBStats(i);
-      }
     }
     if (event.target == dontAnswer){
       console.log("You have decided not to answer");
@@ -55,8 +97,14 @@ function answerSelected (event){
 }
 
 var answerContainer = document.querySelector('.answer-event-box');
-if(answerContainer){
-  answerContainer.addEventListener('click', answerSelected);
+if (answerContainer) {
+    if (window.location.pathname == '/'){
+        answerContainer.addEventListener('click', answerSelectedHP);
+        console.log('Using homepage answer listener');
+    }else{
+        answerContainer.addEventListener('click', answerSelected);
+        console.log('Using answer question answer listener');
+    }
 }
 
 for (var i = 0; i < answerChoices.length; i+=2){
