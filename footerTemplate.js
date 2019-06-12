95c910853c34bdf6d14fff62330fda7325503c0b
Kennan <kaneshke@oregonstate.edu>
2019-06-11 19:38:07 -0700

    Updated

--- footerTemplate.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['footer'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<footer>\n   <div class=\"footer\">\n     <a href=\"https://www.facebook.com\" class=\"fab fa-facebook-square\"></a>\n     <a href=\"https://www.twitter.com\" class=\"fab fa-twitter\"></a>\n     <a href=\"https://www.instagram.com\" class=\"fab fa-instagram\"></a>\n   </div>\n</footer>\n";
-},"useData":true});
-})();
